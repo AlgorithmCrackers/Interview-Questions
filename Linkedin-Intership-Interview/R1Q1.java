4a03b92f947ee32254cf649636b836fbb8adcdb0
Prakash Natarajan <nprakashece@gmail.com>
2014-11-06 15:34:05 -0500

    compiled TODO:Documentation

--- Linkedin-Intership-Interview/R1Q1.java
+++ Linkedin-Intership-Interview/R1Q1.java
@@ -1,6 +1,13 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
-{
-	public int Distance(String[] array, String X, String Y){
+{	//generic type which can be used for any array type
+	public<T> int Distance(T[] array, T X, T Y){	//O(n^2)
 		int l = array.length;
 		int minDist = l;
 		int i = 0;
@@ -10,21 +17,23 @@ class Ideone
 				while(j < l) {
 					Boolean boolA = (X.equals(array[i]) && Y.equals(array[j]));
 					Boolean boolB = (X.equals(array[j]) && Y.equals(array[i]));
-					if((boolA || boolB) && minDist > Math.abs(i-j)) {
+					if((boolA || boolB) && minDist > Math.abs(i-j)) {	//update the minDist
 						minDist = Math.abs(i-j);
 					}
 					j++;
 				}
-				i++;
 			}
+			i++;
 		}
 		return minDist;
 	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Ideone program = new Ideone();
-		String[] array = {"the", "quick", "brown", "fox", "quick"};
-		System.out.println( program.Distance(array, "fox", "the"));
+		String[] arrayS = {"the", "quick", "brown", "fox", "quick"};
+		System.out.println( program.Distance(arrayS, "fox", "the"));	//3
+		Integer[] array = {1, 2, 3, 1};
+		System.out.println( program.Distance(array, 1, 3));	//1
 		
 	}
 }
