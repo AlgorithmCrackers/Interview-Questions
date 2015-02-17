a5be7b8a12d9a3f0d7f48c1235963c2ef0a8ed90
santhosh <santhoshvai@gmail.com>
2015-02-17 12:21:32 +0100

    commented the cpp version

--- Remove-duplicates-from-sorted-array/C++/Solution.cpp
+++ Remove-duplicates-from-sorted-array/C++/Solution.cpp
@@ -4,8 +4,8 @@ public:
         if (n < 2) return n;
         int index = 0;
         for (int i = 1; i < n; i++) {
-            if (A[index] != A[i])
-                A[++index] = A[i];
+            if (A[index] != A[i]) // only if the encountering numbering is unique
+                A[++index] = A[i]; // copy that to the unique array
         }
         return index + 1; // return the length of the unique elements array
     }
