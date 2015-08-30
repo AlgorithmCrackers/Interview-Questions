cbafd06d7b79a914f3b2859055de7a0312a1cd79
Prakash N <nprakashece@gmail.com>
2015-08-30 18:27:35 -0400

    added comments

--- String/checkForUniqueCharacters/Soultion.py
+++ String/checkForUniqueCharacters/Soultion.py
@@ -1,13 +1,13 @@
 
 def isUnique(str):
-	char_set = set()
+	char_set = set() #set to strore all the characters
 	if(len(str) == 1 or len(str) == 0):
 		return True
 	for i in range(len(str)):
-		if str[i] in char_set:
+		if str[i] in char_set:	#if the char is already in set not unique
 			return False
 		else:
-			char_set.add(str[i])
+			char_set.add(str[i])	#add the element into the set
 	return True
 
 
