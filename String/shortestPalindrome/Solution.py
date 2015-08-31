68662e66bfafc8e8dc0067dacffc7e1549b56947
Prakash N <nprakashece@gmail.com>
2015-08-31 16:24:01 -0400

    added cornor cases

--- String/shortestPalindrome/Solution.py
+++ String/shortestPalindrome/Solution.py
@@ -13,6 +13,8 @@ def isPalindrome(str):
 	return True
 
 def shortestPalindrome(str):
+	if len(str) == 0 or len(str) == 1:
+		return str
 	for i in range(len(str)):
 		temp = str + str[0:i][::-1]	# [::-1] is to reverse the string
 		if isPalindrome(temp):
@@ -22,6 +24,10 @@ def shortestPalindrome(str):
 def main():
 	print 'testing Palindrome'
 	assert shortestPalindrome('aaab') == 'aaabaaa'
+	assert shortestPalindrome('a') == 'a'
+	assert shortestPalindrome('aa') == 'aa'
+	assert shortestPalindrome('tim') == 'timit'
+	assert shortestPalindrome('') == ''
 	print 'all is well :)'
 if __name__ == '__main__':
 	main()
