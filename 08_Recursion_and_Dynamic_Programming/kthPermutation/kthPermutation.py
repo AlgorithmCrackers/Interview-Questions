7fe8c99db75769ec76fb734b57af85e0eb68dab3
Prakash N <nprakashece@gmail.com>
2015-09-10 13:08:05 -0400

    removed the recursion problem

--- 08_Recursion_and_Dynamic_Programming/kthPermutation/kthPermutation.py
+++ 08_Recursion_and_Dynamic_Programming/kthPermutation/kthPermutation.py
@@ -4,33 +4,29 @@ class Solution(object):
 
     # Time complexity O(2 ^ n)
     def getPermutation(self, n, k):
-        """
-        :type n: int
-        :type k: int
-        :rtype: str
-        """
-        """
-        Constraints : 1 <= n <= 9
-        k can be anything
-        
-        Idea:
-        1) recursive for permutation.
-        2) when the recursion count reaches k return the current computed permutation
-        """
         char_list = map(str, range(1, n + 1)) #convert to strin
         used = [False] * len(char_list)
         result = []
-        self._getPermutation_helper(result, char_list, used, [])
-        return "".join(str(x) for x in result[k - 1])
+        kthArray = self._getPermutation_helper(result, char_list, used, [], k)
+        print "kth array is"
+        print kthArray #kthArray is always None
+        return kthArray
 
-    def _getPermutation_helper(self, result, char_list, used, cur):
+    def _getPermutation_helper(self, result, char_list, used, cur,  k):
         if len(char_list) == len(cur):
             result.append(cur + [])
+            print len(result)
+            print cur
+        if len(result) == k:
+            print "cur in kth is {0}".format(cur)
+            return cur #cur is printed correctly but not returned
         for i in range(len(char_list)):
             if not used[i]:
                 cur.append(char_list[i])
                 used[i] = True
-                self._getPermutation_helper(result, char_list, used, cur)
+                r = self._getPermutation_helper(result, char_list, used, cur, k)
+                if r is not None:
+                    return r
                 # back track
                 used[i] = False
                 cur.remove(char_list[i])
