87f74d855838b0c2a8770cc0dcf921c102ba2001
prakashn27 <nprakashece@gmail.com>
2015-09-14 11:17:03 -0400

    moved anagram to Strings

--- 01_Strings/isAnagram/Solution.py
+++ 01_Strings/isAnagram/Solution.py
@@ -21,14 +21,71 @@ def isAnagram(str1, str2):
 					del char_dict[str2[i]]
 	return True
 
+def isAnagram_2(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    sLen = len(s)
+    tLen = len(t)
+    if sLen != tLen:
+        return False
+    char_count = [0] * 26
+    for i in range(sLen):
+        char_count[ord(s[i]) - ord('a')] += 1
+    for j in range(tLen):
+        char_count[ord(t[j]) - ord('a')] -= 1
+    for k in range(sLen):
+        if char_count[k] != 0:
+            return False
+    return True
+
+# O(nlog n) for sorting(which is internal)
+# O(1) space
+
+def isAnagram_3(s, t):
+    sLen = len(s)
+    tLen = len(t)
+    if sLen != tLen:
+        return False
+    s = sorted(s)
+    t = sorted(t)
+    for i in range(sLen):
+        if s[i] != t[i]:
+            return False
+    return True
+
+# O(n) time, O(1) space
+def isAnagram_4(s, t):
+    """
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    while s != '':
+        if len(s) != len(t):
+            return False
+        tmp = s[0]
+        s = s.replace(tmp, '')
+        try:
+            t = t.replace(tmp, '')
+        except:
+            return False
+
+    if t != '':
+        return False
+    else:
+        return True
+
 def main():
 	print "running isAnagram"
 	assert isAnagram("anagram","ngarama") == True
-	assert isAnagram("anagram","nagaram") == True
-	assert isAnagram("aaaaa","a") == False
-	assert isAnagram("a","aaaaa") == False
-	assert isAnagram("level","leael") == False
-	assert isAnagram("level","level") == True
+	assert isAnagram_2("anagram","nagaram") == True
+	assert isAnagram_3("aaaaa","a") == False
+	assert isAnagram_4("a","aaaaa") == False
+	assert isAnagram_2("level","leael") == False
+	assert isAnagram_4("level","level") == True
 	print 'All is well :)'
 
 if __name__ == '__main__':
