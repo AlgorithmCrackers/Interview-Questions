c0102f05208e3a627c188d7051b8382cff40cd48
santhosh vaiyapuri <santhoshvai@gmail.com>
2016-09-23 15:29:50 +0200

    replace 2 functions with 1 function, also use size_t

--- 00_Arrays/Count-the-number-of-occurrences-in-a-sorted-array/solution_divideAndConquer.c
+++ 00_Arrays/Count-the-number-of-occurrences-in-a-sorted-array/solution_divideAndConquer.c
@@ -1,7 +1,7 @@
 /*
 Count the number of occurrences in a sorted array
   Given a sorted array arr[] and a number x,
-  write a function that counts the occurrences of x in arr[]. 
+  write a function that counts the occurrences of x in arr[].
 
 http://goo.gl/4cpGcs
 
@@ -10,74 +10,59 @@ Programming Paradigm: Divide & Conquer
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
+#include <stddef.h>
 
-int first(int arr[], int low, int high, int x, int n);
-int last(int arr[], int low, int high, int x, int n);
+int binarySearch(int arr[], int x, size_t n, bool isFirst);
 
-/* if x is present in arr[] then returns the count of occurrences of x, 
+/* if x is present in arr[] then returns the count of occurrences of x,
    otherwise returns -1. */
-int count(int arr[], int x, int n)
+int count(int arr[], int x, size_t n)
 {
   int i; // index of first occurrence of x in arr[0..n-1]
   int j; // index of last occurrence of x in arr[0..n-1]
-     
+
   /* get the index of first occurrence of x */
-  i = first(arr, 0, n-1, x, n);
- 
+  i = binarySearch(arr, x, n, true);
+
   /* If x doesn't exist in arr[] then return -1 */
   if(i == -1)
     return i;
-    
-  /* Else get the index of last occurrence of x. Note that we 
-      are only looking in the subarray after first occurrence */  
-  j = last(arr, i, n-1, x, n);     
-    
+
+  /* Else get the index of last occurrence of x. */
+  j = binarySearch(arr, x, n, false);
+
   /* return count */
   return j-i+1;
 }
- 
-/* if x is present in arr[] then returns the index of FIRST occurrence 
-   of x in arr[0..n-1], otherwise returns -1 */
-int first(int arr[], int low, int high, int x, int n)
+
+int binarySearch(int arr[], int x, size_t n, bool isFirst)
 {
-  if(high >= low)
+  size_t low = 0;
+  size_t high = n-1;
+  size_t result = -1; // initialise result to -1
+  while(low <= high)
   {
-    int mid = (low + high)/2;  /*low + (high - low)/2;*/
-    if ( ( mid == 0 ) || (  ( x > arr[mid-1] )  && ( arr[mid] == x ) ) )
-      return mid;
+    int mid = low + (high - low)/2;
+    if (arr[mid] == x) {
+      result = mid;
+      if(isFirst) high = mid-1; // need first occurence
+      else low = mid+1; //  need second occurence
+    }
     else if(x > arr[mid])
-      return first(arr, (mid + 1), high, x, n);
+      low = mid + 1;
     else
-      return first(arr, low, (mid -1), x, n);
+      high = mid - 1;
   }
-  return -1;
+  return result;
 }
- 
- 
-/* if x is present in arr[] then returns the index of LAST occurrence 
-   of x in arr[0..n-1], otherwise returns -1 */
-int last(int arr[], int low, int high, int x, int n)
-{
-  if(high >= low)
-  {
-    int mid = (low + high)/2;  /*low + (high - low)/2;*/
-    if ( ( mid == n-1 ) || ( ( x < arr[mid+1])  && ( arr[mid] == x ) ) )
-      return mid;
-    else if(x < arr[mid])
-      return last(arr, low, (mid -1), x, n);
-    else
-      return last(arr, (mid + 1), high, x, n);    
-  }
-  return -1;
-}
- 
+
 /* driver program to test above functions */
 int main()
 {
   int arr[] = {1, 2, 2, 3, 3, 3, 3};
   int x =  3;  // Element to be counted in arr[]
-  int n = sizeof(arr)/sizeof(arr[0]);
+  size_t n = sizeof(arr)/sizeof(arr[0]);
   int c = count(arr, x, n);
   printf(" \"%d\" occurs %d times ", x, c);
   getchar();
