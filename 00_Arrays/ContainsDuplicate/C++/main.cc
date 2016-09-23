7c561b5ffdc64659492ee40d14b707ca0470581e
santhosh vaiyapuri <santhoshvai@gmail.com>
2016-09-23 16:10:40 +0200

    Adding c++ version

---
+++ 00_Arrays/ContainsDuplicate/C++/main.cc
@@ -0,0 +1,28 @@
+#include <unordered_map>
+#include <iostream>
+#include <cassert>
+
+bool containsDuplicate(int nums[], size_t size, size_t k) {
+  std::unordered_map<int, size_t> m;
+  for(size_t i=0; i < size; i++) {
+    if(m.find(nums[i]) == m.end()) {
+      // new element
+      m[ nums[i] ] = i;
+    }
+    else {
+      size_t prev_index = m[ nums[i] ];
+      if ( (i - prev_index) <= k ) return true;
+      else m[ nums[i] ] = i;
+    }
+  }
+  return false;
+}
+
+int main(void)
+{
+  int arr[] = {1, 2, 7, 2, 3, 4};
+  size_t n = sizeof(arr)/sizeof(arr[0]);
+  assert(containsDuplicate(arr, n, 2) == true);
+  assert(containsDuplicate(arr, n, 1) == false);
+  return 0;
+}
--- 00_Arrays/ContainsDuplicate/README.md
+++ 00_Arrays/ContainsDuplicate/README.md
@@ -1,3 +1,7 @@
 ## Contains Duplicate
 
 Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the difference between i and j is at most k.
+
+## Solution
+
+Use Hashmap to store the indices of occurrences. Then find if it occurs again.
