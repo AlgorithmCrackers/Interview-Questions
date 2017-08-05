bf85d7327a4dd240025c9a8462a66757bf68f5cd
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2017-08-05 12:36:06 +0200

    Update Solution.java

--- 13_Misc/ReverseInteger/Solution.java
+++ 13_Misc/ReverseInteger/Solution.java
@@ -7,12 +7,12 @@ import java.io.*;
 class Main
 {
 	public static int reverseInt(int x) {
-	  int rev = 0;
-	  while(x != 0){
-	    rev = rev*10 + x%10;
-	    x = x/10;
-	  } 
-	  return rev;
+		int rev = 0;
+	  	while(x != 0){
+			rev = rev*10 + x%10;
+			x = x/10;
+	  	} 
+	  	return rev;
 	}
 	
 	public static void main (String[] args) throws java.lang.Exception
@@ -20,10 +20,10 @@ class Main
 		System.out.println(reverseInt(321));
 		System.out.println(reverseInt(-321));
 		System.out.println(reverseInt(0));
-    /*
-    123
-    -123
-    0
-    */
+		/*
+		123
+		-123
+		0
+		*/
 	}
 }
