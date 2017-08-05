710372cd16c91c4a3340e09566f65d61ace180cb
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2017-08-05 12:35:05 +0200

    Create Solution.java

---
+++ 13_Misc/ReverseInteger/Solution.java
@@ -0,0 +1,29 @@
+package info.santhosh.interview;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main
+{
+	public static int reverseInt(int x) {
+	  int rev = 0;
+	  while(x != 0){
+	    rev = rev*10 + x%10;
+	    x = x/10;
+	  } 
+	  return rev;
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println(reverseInt(321));
+		System.out.println(reverseInt(-321));
+		System.out.println(reverseInt(0));
+    /*
+    123
+    -123
+    0
+    */
+	}
+}
