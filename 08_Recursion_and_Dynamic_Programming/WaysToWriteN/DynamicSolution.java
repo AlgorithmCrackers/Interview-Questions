2f9660726e458d01b7ff239d80cfa45eb6b07841
Prakash Natarajan <nprakashece@gmail.com>
2017-11-28 16:43:04 -0500

    Create DynamicSolution.java

---
+++ 08_Recursion_and_Dynamic_Programming/WaysToWriteN/DynamicSolution.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	static int[] res;
+	static int[] arr;
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n = 40;
+		res = new int[n+1];
+		arr = new int[] {1, 3, 4};
+		for(int i = 0; i <= n; i++) {
+			sol(n);
+		}
+		System.out.println(res[n]);	
+	}
+	static int sol(int n) {
+		if(n < 0) return 0;
+		if(n == 0) return 1;
+		if(res[n] != 0) return res[n];
+		int r = 0;
+		for (int a : arr) {
+			r += sol(n-a);
+		}
+		res[n] = r;
+		return r;
+	}
+	
+}
