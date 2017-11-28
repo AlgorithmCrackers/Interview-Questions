0b9371230c3eef5354110838de025dfe36429eea
Prakash Natarajan <nprakashece@gmail.com>
2017-11-28 16:43:21 -0500

    Update DynamicSolution.java

--- 08_Recursion_and_Dynamic_Programming/WaysToWriteN/DynamicSolution.java
+++ 08_Recursion_and_Dynamic_Programming/WaysToWriteN/DynamicSolution.java
@@ -4,8 +4,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class DynamicSolution
 {
 	static int[] res;
 	static int[] arr;
