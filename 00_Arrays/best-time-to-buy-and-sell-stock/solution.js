0f501b07f55c419ba69fe13ddcb6b382f7a9a2e4
Santhosh Vaiyapuri <3846977+santhoshvai@users.noreply.github.com>
2020-01-08 21:09:42 +0100

    Add best-time-to-buy-and-sell-stock solution

---
+++ 00_Arrays/best-time-to-buy-and-sell-stock/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minSoFar = Number.MAX_SAFE_INTEGER
+    let maxProfit = 0;
+    prices.forEach((value, index) => {
+        if (value < minSoFar) {
+            minSoFar = value
+        } else {
+            const profit = value - minSoFar
+            maxProfit = Math.max(maxProfit, profit)
+        }
+    })
+    return maxProfit;
+};
