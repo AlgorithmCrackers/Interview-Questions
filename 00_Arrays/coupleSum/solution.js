75af29497c61ccefdde62e928ffa36c13838eb7a
Santhosh Vaiyapuri <3846977+santhoshvai@users.noreply.github.com>
2020-10-02 09:32:04 +0200

    add 1 pass hashmap for 2 sum

--- 00_Arrays/coupleSum/solution.js
+++ 00_Arrays/coupleSum/solution.js
@@ -1,9 +1,5 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-const twoSum = function(nums, target) {
+// two pass hashmap solution
+const twoSum2 = function(nums, target) {
     const cache = {}
     // put a map of <value, index>
     nums.forEach((value, index) => {
@@ -17,6 +13,22 @@ const twoSum = function(nums, target) {
             const j = cache[needed]
             if (i !== j) return [i, j]
         }
+    }
+    return []
+};
+
+// 1 pass hashmap solution
+var twoSum = function(nums, target) {
+    // put a map of <value, index>
+    const cache = {}
+    // iterate through array and check if we had the needed value in the hashmap
+    for (let i=0; i< nums.length; i++) {
+        const needed = target - nums[i]
+        if (needed in cache) {
+            const j = cache[needed]
+            return [i, j]
+        }
+        cache[nums[i]] = i
     }
     return []
 };
